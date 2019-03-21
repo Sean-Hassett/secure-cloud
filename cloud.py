55207660e8c1a9195b0c43ee0f8170ce7b334f6d
Sean-Hassett <hassetse@tcd.ie>
2019-03-21 12:38:34 +0000

    refactoring

--- cloud.py
+++ cloud.py
@@ -9,7 +9,7 @@ def process_folder_entries(current_state, entries):
         if isinstance(entry, dropbox.files.FileMetadata):
             current_state[entry.path_lower] = entry
         elif isinstance(entry, dropbox.files.DeletedMetadata):
-            current_state.pop(entry.path_lower, None)  # ignore KeyError if missing
+            current_state.pop(entry.path_lower, None)
     return current_state
 
 
@@ -29,4 +29,5 @@ while result.has_more:
     result = dbx.files_list_folder_continue(result.cursor)
     files = process_folder_entries(files, result.entries)
 
-print(files)
+for file in files:
+    print(file[1:])
